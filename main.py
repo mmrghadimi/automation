70ae6a4f5e635136a5194fb47b52b2bfc7e83a52
MohammadReza Ghadimi <mmr.ghadimi@gmail.com>
2021-10-07 01:43:23 +0330

    upgrade of the code for multiple input files

--- main.py
+++ main.py
@@ -1,30 +1,30 @@
 import openpyxl as xl
 from openpyxl.chart import BarChart, Reference
 
-wb = xl.load_workbook('file.xlsx')
 
-sheet = wb['Sheet1']
-cell = sheet['A1']
-print(cell.value)
-cell = sheet.cell(1, 1)
-print(cell.value)
-print(sheet.max_row)
+def process_wb(filename):
+    wb = xl.load_workbook(filename)
+    sheet = wb['Sheet1']
 
-for row in range(2, sheet.max_row + 1):
-    cell = sheet.cell(row, 3)
-    c1 = 0.9 * cell.value
-    c2 = sheet.cell(row, 4)
-    c2.value = c1
+    for row in range(2, sheet.max_row + 1):
+        cell = sheet.cell(row, 3)
+        c1 = 0.9 * cell.value
+        c2 = sheet.cell(row, 4)
+        c2.value = c1
 
-values = Reference(sheet,
-                   min_row=2,
-                   max_row=sheet.max_row,
-                   min_col=4,
-                   max_col=4,
-                   )
+    values = Reference(sheet,
+                       min_row=2,
+                       max_row=sheet.max_row,
+                       min_col=4,
+                       max_col=4,
+                       )
 
-chart = BarChart()
-chart.add_data(values)
-sheet.add_chart(chart, 'E2')
+    chart = BarChart()
+    chart.add_data(values)
+    sheet.add_chart(chart, 'E2')
 
-wb.save('file2.xlsx')
+    filename2 = filename.split('.')[0] + '2.' + filename.split('.')[1]
+    wb.save(filename2)
+
+
+process_wb('file.xlsx')
