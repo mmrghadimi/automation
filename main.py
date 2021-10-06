33ad19c6a2a1335ed880e9848f8d6ef0e453881f
MohammadReza Ghadimi <mmr.ghadimi@gmail.com>
2021-10-07 01:27:07 +0330

    adding a chart to worksheet

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import openpyxl as xl
+from openpyxl.chart import BarChart, Reference
 
 wb = xl.load_workbook('file.xlsx')
 
@@ -12,8 +13,18 @@ print(sheet.max_row)
 for row in range(2, sheet.max_row + 1):
     cell = sheet.cell(row, 3)
     c1 = 0.9 * cell.value
-    c2 = sheet.cell(row,4)
+    c2 = sheet.cell(row, 4)
     c2.value = c1
 
-wb.save('file2.xlsx')
+values = Reference(sheet,
+                   min_row=2,
+                   max_row=sheet.max_row,
+                   min_col=4,
+                   max_col=4,
+                   )
 
+chart = BarChart()
+chart.add_data(values)
+sheet.add_chart(chart, 'E2')
+
+wb.save('file2.xlsx')
